f84084878f671f9ac102cde0d08068ea4a6a0cd6
Kenny Molina <kjmolina@live.com>
2022-11-28 14:17:53 -0600

    update

--- src/components/atoms/CloseIcon.jsx
+++ src/components/atoms/CloseIcon.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CloseIcon = ({ event }) => (
-    <div className="hamburger ">
-        <a
-            href="#"
-            onClick={event}
-        >
-            <div className="hamburger-inner right-3 top-7"></div>
-        </a>
+  <div className="hamburger ">
+    <div onClick={event}>
+      <div className="hamburger-inner right-3 top-7"></div>
     </div>
-)
+  </div>
+);
 
-
-export default CloseIcon
+export default CloseIcon;
--- src/views/chats/Chats.jsx
+++ src/views/chats/Chats.jsx
@@ -1,27 +1,74 @@
-import React, { useContext, useState } from 'react'
-import { UserContext } from '../../hooks/userContext';
-import { notification, } from 'antd';
-import { my_fetch } from '../../utils/fetch';
-import MainLayout from '../layouts/MainLayout';
-import { screenSize } from '../../const/screensize';
-import SmallDeviceView from './SmallDeviceView';
-import MediumDeviceView from './MediumDeviceView';
-
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../hooks/userContext";
+import { notification } from "antd";
+import { my_fetch } from "../../utils/fetch";
+import MainLayout from "../layouts/MainLayout";
+import { screenSize } from "../../const/screensize";
+import SmallDeviceView from "./SmallDeviceView";
+import MediumDeviceView from "./MediumDeviceView";
 
 const Chats = () => {
-    const [showModal, setshowModal] = useState(false)
-    const userLogged = useContext(UserContext);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [userName, setuserName] = useState('');
-    const [showUsers, setshowUsers] = useState(true)
-    return (
-        <MainLayout>
-            <div className='h-full grid-container ' >
-                {(screenSize.medium < window.innerWidth) && (<MediumDeviceView showModal={showModal} setshowModal={setshowModal} />)}
-                {(screenSize.medium > window.innerWidth) && (<SmallDeviceView showUser={showUsers} />)}
-            </div>
-        </MainLayout >
-    )
-}
+  const { userLogged } = useContext(UserContext);
+  const [showModal, setshowModal] = useState(false);
+  const [userNameAdd, setuserNameAdd] = useState("");
+  const [userFriends, setuserFriends] = useState([]);
+  const [showUsers, ] = useState(true);
+  const addFriend = async () => {
+    const answer = await my_fetch.my_fetch_post(
+      `${process.env.REACT_APP_API_URL}/users/addFriend`,
+      {
+        userName: userNameAdd,
+        userLoggedId: userLogged._id,
+      }
+    );
+    notification.open({
+      description: answer.message,
+    });
+    if (answer.success) {
+      const answer2 = await my_fetch.my_fetch_post(
+        `${process.env.REACT_APP_API_URL}/users/getfriends`,
+        {
+          userId: userLogged._id,
+        }
+      );
+      setuserFriends(answer2.data);
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      const answer2 = await my_fetch.my_fetch_post(
+        `${process.env.REACT_APP_API_URL}/users/getfriends`,
+        {
+          userId: userLogged._id,
+        }
+      );
+      setuserFriends(answer2.data);
+    })();
 
-export default Chats
+    return () => {
+        setuserFriends([]);
+    };
+  }, [userLogged._id]);
+
+  return (
+    <MainLayout>
+      <div className="h-full grid-container ">
+        {screenSize.medium < window.innerWidth && (
+          <MediumDeviceView
+            showModal={showModal}
+            setshowModal={setshowModal}
+            userNameAdd={userNameAdd}
+            setuserNameAdd={setuserNameAdd}
+            addFriend={addFriend}
+            userFriends={userFriends}
+          />
+        )}
+        {screenSize.medium > window.innerWidth && (
+          <SmallDeviceView showUser={showUsers} />
+        )}
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Chats;
