df5c8da8293ef28d7f92ba7e83eafa29794260dc
Kenny Molina <kjmolina@live.com>
2022-12-29 18:53:55 -0600

    add

--- src/views/CreateUser/CreateUser.jsx
+++ src/views/CreateUser/CreateUser.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { my_fetch } from "../../utils/fetch";
 import { UserContext } from "../../hooks/userContext";
-import { notification } from "antd";
+import { Col, notification, Row, Space } from "antd";
 import LogIn from "./LogIn";
 import Registry from "./Registry";
 const isForm = ["registry", "login"];
@@ -13,8 +13,29 @@ const CreateUser = () => {
   const [form, setform] = useState(isForm[1]);
   const [userName, setUserName] = useState("");
   const [password, setpassword] = useState("");
+  const [newUser, setnewUser] = useState({
+    name: "",
+    lastName: "",
+    userName: "",
+    description: "",
+    password: "",
+  });
   const addUser = async () => {
     setform(isForm[0]);
+  };
+  const handleCreateUser = async () => {
+    const answer = await my_fetch.my_fetch_post(
+      `${process.env.REACT_APP_API_URL}/users`,
+      newUser
+    );
+    if (answer?.success) {
+      setUserLogged(answer.data);
+      navigate("/chats");
+    } else
+      notification.open({
+        message: "Error",
+        description: answer?.message,
+      });
   };
   const logUser = async () => {
     const answer = await my_fetch.my_fetch_post(
@@ -31,22 +52,32 @@ const CreateUser = () => {
     });
   };
   return (
-    <div className="flex flex-row h-full chatBackground">
-      <div className="p-5 bg-white h-full grid content-center w-3/12">
-        {form === isForm[1] ? (
-          <LogIn
-            addUser={addUser}
-            logUser={logUser}
-            password={password}
-            setUserName={setUserName}
-            setpassword={setpassword}
-            userName={userName}
-          />
-        ) : (
-          <Registry handleChangeLogIn={()=>{setform(isForm[1])}} />
-        )}
-      </div>
-      <div className="pl-20 pt-10">{/* TODO */}</div>
+    <div className="h-full chatBackground ">
+      <Row className="h-full">
+        <Col md={10} sm={24} className="md:bg-white bg-white/70 h-full flex justify-center p-10">
+          <Space align="center">
+            {form === isForm[1] ? (
+              <LogIn
+                addUser={addUser}
+                logUser={logUser}
+                password={password}
+                setUserName={setUserName}
+                setpassword={setpassword}
+                userName={userName}
+              />
+            ) : (
+              <Registry
+                handleChangeLogIn={() => {
+                  setform(isForm[1]);
+                }}
+                handleCreateUser={handleCreateUser}
+                newUser={newUser}
+                setnewUser={setnewUser}
+              />
+            )}
+          </Space>
+        </Col>
+      </Row>
     </div>
   );
 };
