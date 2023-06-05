51284c03e0c90ab13a745acd1364832ca5d06ba5
kenny molina <kenny.molina.murillo@gmail.com>
2023-06-05 09:14:37 -0600

    commit

--- src/views/blogForm/BlogForm.jsx
+++ src/views/blogForm/BlogForm.jsx
@@ -12,7 +12,7 @@ import {
 import React, { useState } from "react";
 ///////////////////////
 import MainLayout from "../layouts/MainLayout";
-
+//////////////////////////asdsd 
 const { TextArea } = Input;
 
 const BlogForm = () => {
